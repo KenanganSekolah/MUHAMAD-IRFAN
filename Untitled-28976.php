3c8e9f5417154b92f42e6443211d394c316b3986
KenanganSekolah <129031408+KenanganSekolah@users.noreply.github.com>
2023-03-27 12:27:14 +0700

    Add files via upload

---
+++ Untitled-28976.php
@@ -0,0 +1,16 @@
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$nama = $_POST["nama"];
+$email = $_POST["email"];
+$pesan = $_POST["pesan"];
+
+$to = "mirfan260626@gmail.com";
+$subject = "pesan dari $nama" ;
+$message = "Email: $email\n\npesan:\n$pesan";
+$headers = "from:";
+
+if (mail($to, $subject $message, $headers)) {
+echo "<p>pesan berhasil terkirim.</p>"
+} else {
+echo "<p>maaf, terjadi kesalahan saat mengirim pesan.</p>"
+}
+}
